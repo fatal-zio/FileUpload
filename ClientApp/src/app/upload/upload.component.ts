321e660c835209992e8c8add5c042ec9ee349180
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2018-12-04 16:19:16 -0600

    Initial working version.

---
+++ ClientApp/src/app/upload/upload.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpRequest, HttpEventType, HttpResponse } from '@angular/common/http';
+
+@Component({
+  selector: 'app-upload',
+  templateUrl: './upload.component.html',
+  styleUrls: ['./upload.component.css']
+})
+export class UploadComponent implements OnInit {
+  public progress: number;
+  public message: string;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+  }
+
+  upload(files) {
+    if (files.length === 0) {
+      return;
+    }
+
+    const formData = new FormData();
+
+    for (const file of files) {
+      formData.append(file.name, file);
+    }
+
+    const uploadReq = new HttpRequest('POST', `api/upload`, formData, {
+      reportProgress: true,
+    });
+
+    this.http.request(uploadReq).subscribe(event => {
+      if (event.type === HttpEventType.UploadProgress) {
+        this.progress = Math.round(100 * event.loaded / event.total);
+      } else if (event.type === HttpEventType.Response) {
+        this.message = event.body.toString();
+             }
+    });
+  }
+}
---
+++ Controllers/UploadController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileUpload.Controllers
+{
+    [Route("api/upload")]
+    public class UploadController : ControllerBase
+    {
+        private IHostingEnvironment _hostingEnvironment;
+
+        public UploadController(IHostingEnvironment hostingEnvironment) => _hostingEnvironment = hostingEnvironment;
+
+        [HttpPost, DisableRequestSizeLimit]
+        public IActionResult UploadFile()
+        {
+            try
+            {
+                var file = Request.Form.Files[0];
+                string folderName = "Upload";
+                string webRootPath = _hostingEnvironment.WebRootPath;
+                string newPath = Path.Combine(webRootPath, folderName);
+
+                if (!Directory.Exists(newPath))
+                {
+                    Directory.CreateDirectory(newPath);
+                }
+
+                if (file.Length > 0)
+                {
+                    string fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    string fullPath = Path.Combine(newPath, fileName);
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                }
+                
+                return Ok("Upload Successful.");
+            }
+            catch (System.Exception ex)
+            {
+                return StatusCode(500, "Upload Failed: " + ex.Message);
+            }
+        }
+    }
+}
+
